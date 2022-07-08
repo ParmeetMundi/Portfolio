eaf8b54b7069340e344858c0a1d82340df64cc29
Parmeet Singh <parmeet845@gmail.com>
2022-07-08 17:43:50 +0530

    cgpa updated

--- src/Achivements.js
+++ src/Achivements.js
@@ -5,8 +5,8 @@ const Achivements = () => {
     return <div class='Achivements'>
      <p id='achivements'>Achievements</p>
     <ul class="points">
-       <a href='https://www.codechef.com/users/parm_singh'> <li>Codechef 5 star (max rating 2064) <img src='link.png'/></li> </a>
-       <a href='https://codeforces.com/profile/Parmeet_Singh'> <li>Codeforces Expert (max rating 1672)<img src='link.png'/></li> </a>
+       <a href='https://www.codechef.com/users/parm_singh'> <li>Codechef 5 star (max rating 2064) <img src='link.webp'/></li> </a>
+       <a href='https://codeforces.com/profile/Parmeet_Singh'> <li>Codeforces Expert (max rating 1672)<img src='link.webp'/></li> </a>
         <li>54th Rank in Scaler CodeX 2.0 Contest From 12500 participants</li>
         <li>51st Rank in CodeChef (Division 2) Contest From 3581 participants</li>
         <li>148th Rank in CodeChef August Cook-Off 2021 Division 2 Contest From 3518 participants</li>
